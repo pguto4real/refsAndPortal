f53ce3716bccb0fe2a5f961b3fff019de8d56cf8
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-15 09:11:35 -0600

    old react version

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-
-const ResultModal = ({ result, targetTime,ref }) => {
+import { forwardRef } from "react";
+const ResultModal = forwardRef(({ result, targetTime }, ref) => {
   return (
     <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
@@ -17,6 +17,6 @@ const ResultModal = ({ result, targetTime,ref }) => {
       </form>
     </dialog>
   );
-};
-
+}
+)
 export default ResultModal;
