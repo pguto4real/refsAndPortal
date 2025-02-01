30ee5caccbe222809c16821fe1a34765ac30650f
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-31 23:39:44 -0600

    paragraph

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Paragraph = ({label,type}) => {
+  return (
+    <p>
+    <label>Name</label>
+    <input type="text" />
+  </p>
+  )
+}
+
+export default Paragraph
